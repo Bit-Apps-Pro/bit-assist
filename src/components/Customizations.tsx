83c428dcd5192e9d00c38caf7b7fd79c084dce93
Shuvo Mohajan <shuvomohajan@gmail.com>
2022-05-22 13:30:04 +0600

    button size component added

--- src/components/Customizations.tsx
+++ src/components/Customizations.tsx
@@ -1,6 +1,7 @@
 import {
   Box,
   Button,
+  HStack,
   Input,
   Menu,
   MenuButton,
@@ -8,14 +9,25 @@ import {
   MenuList,
   Stack,
   Text,
+  useRadioGroup,
 } from '@chakra-ui/react'
 import ColorPicker from '@atomik-color/component'
 import { str2Color } from '@atomik-color/core'
 import { useState } from 'react'
 import transparentBg from '../../public/transparent_bg.png'
+import RadioCard from './RadioCard'
 
 const Customizations = () => {
   const [color, setColor] = useState(str2Color('#121212'))
+  const options = ['react', 'vue', 'svelte']
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'framework',
+    defaultValue: 'react',
+    onChange: console.log,
+  })
+
+  const group = getRootProps()
 
   return (
     <Stack gap="10">
@@ -23,14 +35,20 @@ const Customizations = () => {
         <Text mb="2">Button Name</Text>
         <Input placeholder="Button Name" isRequired={true} />
       </Box>
+
       <Box>
         <Text mb="2">Button Color</Text>
         <Menu>
-          <MenuButton bgImage={transparentBg.src} transition='none' rounded='md' overflow='hidden'>
-            <Box bgColor={color.str} h='12' w='12'></Box>
+          <MenuButton
+            bgImage={transparentBg.src}
+            transition="none"
+            rounded="md"
+            overflow="hidden"
+          >
+            <Box bgColor={color.str} h="12" w="12"></Box>
           </MenuButton>
-          <MenuList p="0" border="0" maxW='220px'>
-            <Box maxW='100%'>
+          <MenuList p="0" border="0" maxW="220px">
+            <Box maxW="100%">
               <ColorPicker
                 showParams={true}
                 value={color}
@@ -39,6 +57,20 @@ const Customizations = () => {
             </Box>
           </MenuList>
         </Menu>
+      </Box>
+
+      <Box>
+        <Text mb="2">Button Size</Text>
+        <HStack {...group}>
+          {options.map((value) => {
+            const radio = getRadioProps({ value })
+            return (
+              <RadioCard key={value} {...radio}>
+                {value}
+              </RadioCard>
+            )
+          })}
+        </HStack>
       </Box>
     </Stack>
   )
