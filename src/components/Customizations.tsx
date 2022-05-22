93582089ac2b5d914e4374f26c475d60a8795279
Shuvo Mohajan <shuvomohajan@gmail.com>
2022-05-22 12:53:50 +0600

    added transparent color visualization in widget button

--- src/components/Customizations.tsx
+++ src/components/Customizations.tsx
@@ -12,9 +12,10 @@ import {
 import ColorPicker from '@atomik-color/component'
 import { str2Color } from '@atomik-color/core'
 import { useState } from 'react'
+import transparentBg from '../../public/transparent_bg.png'
 
 const Customizations = () => {
-  const [color, setColor] = useState(str2Color('#000'))
+  const [color, setColor] = useState(str2Color('#121212'))
 
   return (
     <Stack gap="10">
@@ -25,7 +26,9 @@ const Customizations = () => {
       <Box>
         <Text mb="2">Button Color</Text>
         <Menu>
-          <MenuButton h='12' w='12' bg={`#${color.hex}`} transition='none' rounded='md'></MenuButton>
+          <MenuButton bgImage={transparentBg.src} transition='none' rounded='md' overflow='hidden'>
+            <Box bgColor={color.str} h='12' w='12'></Box>
+          </MenuButton>
           <MenuList p="0" border="0" maxW='220px'>
             <Box maxW='100%'>
               <ColorPicker
