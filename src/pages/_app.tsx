1e1e3584bb80a72394ec95e12fad41b02873c664
Arif Uddin <arifunctg@gmail.com>
2022-05-22 13:22:37 +0600

    update _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react'
-
+import '@fontsource/outfit/200.css'
+import '@fontsource/outfit/300.css'
+import '@fontsource/outfit/400.css'
+import '@fontsource/outfit/500.css'
+import '@fontsource/outfit/600.css'
 import theme from '../theme'
 import { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const queryClient = new QueryClient()
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </QueryClientProvider>
   )
 }
-
-export default MyApp
