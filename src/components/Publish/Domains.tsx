89a81336bab7fe3d5bb9a14249522675d62f6476
Shuvo Mohajan <shuvomohajan@gmail.com>
2022-05-28 15:59:41 +0600

    add domain done

--- src/components/Publish/Domains.tsx
+++ src/components/Publish/Domains.tsx
@@ -1,19 +1,31 @@
+/* eslint-disable react/no-children-prop */
 import {
   Box,
   Button,
-  Divider,
   HStack,
   IconButton,
   Input,
   Text,
   useToast,
+  Tooltip,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverArrow,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  Center,
+  useDisclosure,
 } from '@chakra-ui/react'
 import ResponseToast from '@components/Global/ResponseToast'
 import { widgetAtom } from '@globalStates/atoms'
 import useUpdateWidget from '@hooks/mutations/useUpdateWidget'
 import { useAtom } from 'jotai'
-import { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { HiCheck, HiOutlineTrash, HiPlus } from 'react-icons/hi'
+import { Kbd } from '@chakra-ui/react'
 
 const Domains = () => {
   const toast = useToast({ isClosable: true })
@@ -22,23 +34,78 @@ const Domains = () => {
   const [isAdding, setIsAdding] = useState(false)
   const [domainName, setDomainName] = useState('')
 
-  const handleRemoveDomain = async (domainId: number) => {
-    console.log('domainId', domainId)
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.repeat) {
+      return
+    }
+
+    if (e.key === 'Enter') {
+      addNewDomain()
+    } else if (e.key === 'Escape') {
+      resetStates()
+    }
   }
 
   const addNewDomain = async () => {
+    const pattern =
+      /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/gm
+    if (domainName === '' || pattern.test(domainName) === false) {
+      toast({
+        status: 'error',
+        position: 'top-right',
+        description: 'Please enter a valid domain name',
+      })
+      return
+    }
+
+    const domainExists = widget.domains.find(
+      (domain: string) => domain === domainName
+    )
+    if (domainExists) {
+      toast({
+        status: 'warning',
+        position: 'top-right',
+        description: 'Domain already exists',
+      })
+      return
+    }
+
     setWidget((prev) => {
       prev.domains.push(domainName)
     })
     resetStates()
 
-    // const response = await updateWidget()
-    // ResponseToast({
-    //   toast,
-    //   response,
-    //   action: 'create',
-    //   messageFor: 'Widget position',
-    // })
+    const response = await updateWidget({
+      ...widget,
+      domains: [...widget.domains, domainName],
+    })
+    ResponseToast({
+      toast,
+      response,
+      action: 'create',
+      messageFor: 'Widget domain',
+    })
+  }
+
+  const handleRemoveDomain = async (domainIndex: number, onClose: Function) => {
+    const newDomains = [...widget.domains]
+    newDomains.splice(domainIndex, 1)
+    
+    onClose()
+    const response = await updateWidget({
+      ...widget,
+      domains: [...newDomains],
+    })
+    ResponseToast({
+      toast,
+      response,
+      action: 'delete',
+      messageFor: 'Widget domain',
+    })
+
+    setWidget((prev) => {
+      prev.domains.splice(domainIndex, 1)
+    })
   }
 
   const resetStates = () => {
@@ -46,9 +113,15 @@ const Domains = () => {
     setIsAdding(false)
   }
 
+  const initRef = useRef()
+
   return (
     <Box width={'sm'}>
-      <Box mb="4" borderWidth={`${widget.domains.length && '1px'}`}>
+      <Box
+        mb="4"
+        rounded={'md'}
+        borderWidth={`${widget.domains.length && '1px'}`}
+      >
         {widget.domains.map((domain, index) => (
           <HStack
             key={index}
@@ -59,43 +132,81 @@ const Domains = () => {
             borderBottomWidth={`${index < widget.domains.length - 1 && '1px'}`}
           >
             <Text>{domain}</Text>
-            <IconButton
-              isRound={true}
-              aria-label="Remove Domain"
-              variant="ghost"
-              colorScheme="red"
-              icon={<HiOutlineTrash />}
-              onClick={() => handleRemoveDomain(index)}
-            />
+            <Popover closeOnBlur={false} initialFocusRef={initRef}>
+              {({ isOpen, onClose }) => (
+                <>
+                  <PopoverTrigger>
+                    <Box>
+                      <Tooltip label="Remove domain" placement="right">
+                        <IconButton
+                          isRound={true}
+                          aria-label="Remove Domain"
+                          variant="ghost"
+                          colorScheme="red"
+                          icon={<HiOutlineTrash />}
+                          disabled={isWidgetUpdating}
+                        />
+                      </Tooltip>
+                    </Box>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverBody>
+                      <Text>Are you sure you want to remove this domain?</Text>
+                      <Button
+                        mt={4}
+                        colorScheme={'red'}
+                        ref={initRef}
+                        onClick={() => handleRemoveDomain(index, onClose)}
+                        disabled={isWidgetUpdating}
+                      >
+                        Confirm
+                      </Button>
+                    </PopoverBody>
+                  </PopoverContent>
+                </>
+              )}
+            </Popover>
           </HStack>
         ))}
       </Box>
 
       {isAdding && (
         <Box mb={4}>
-          <HStack>
+          <HStack mb={2}>
             <Input
               placeholder="Domain Name"
               value={domainName}
               onChange={(e) => setDomainName(e.target.value)}
+              onKeyDown={handleKeyDown}
+              autoFocus
             />
-            <IconButton
-              isRound={true}
-              aria-label="Remove Domain"
-              variant="ghost"
-              colorScheme="red"
-              icon={<HiOutlineTrash />}
-              onClick={resetStates}
-            />
-            <IconButton
-              isRound={true}
-              aria-label="Remove Domain"
-              variant="ghost"
-              colorScheme="green"
-              icon={<HiCheck />}
-              onClick={() => addNewDomain()}
-            />
+            <Tooltip label="Cancel">
+              <IconButton
+                isRound={true}
+                aria-label="Remove Domain"
+                variant="ghost"
+                colorScheme="red"
+                icon={<HiOutlineTrash />}
+                onClick={resetStates}
+              />
+            </Tooltip>
+            <Tooltip label="Save">
+              <IconButton
+                isRound={true}
+                aria-label="Remove Domain"
+                variant="ghost"
+                colorScheme="green"
+                icon={<HiCheck />}
+                onClick={() => addNewDomain()}
+                disabled={isWidgetUpdating}
+              />
+            </Tooltip>
           </HStack>
+          <span>
+            <Kbd>enter</Kbd> to add, &nbsp; <Kbd>esc</Kbd> to cancel
+          </span>
         </Box>
       )}
 
@@ -106,6 +217,7 @@ const Domains = () => {
           variant="outline"
           rounded={'full'}
           onClick={() => setIsAdding(true)}
+          isLoading={isWidgetUpdating}
         >
           Add Domain
         </Button>
