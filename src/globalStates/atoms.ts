11a4ab46a8afa8dea5d80d30376e0f4ac91f3e2a
Azizur Rahman Jishat <48252409+jishat@users.noreply.github.com>
2022-05-30 16:28:39 +0600

    declare state for user during signup

--- src/globalStates/atoms.ts
+++ src/globalStates/atoms.ts
@@ -1,4 +1,7 @@
 import { chat_widgets } from '@prisma/client'
+import { atom } from 'jotai'
 import { atomWithImmer } from 'jotai/immer'
 
 export const widgetAtom = atomWithImmer<chat_widgets>()
+
+export const userState = atom({})
