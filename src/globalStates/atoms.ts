ed5c61fb96161e5a2ef69cd8135d24eebeb9b8a2
Azizur Rahman Jishat <48252409+jishat@users.noreply.github.com>
2022-06-04 10:28:44 +0600

    add data type in userState

--- src/globalStates/atoms.ts
+++ src/globalStates/atoms.ts
@@ -2,6 +2,16 @@ import { chat_widgets } from '@prisma/client'
 import { atom } from 'jotai'
 import { atomWithImmer } from 'jotai/immer'
 
+interface UserStateType {
+  name?: string
+  email?: string
+  puid?: string
+  enableAffiliate?: null | boolean
+  referId?: string
+  role?: string
+  _id?: string
+}
+
 export const widgetAtom = atomWithImmer<chat_widgets>()
 
-export const userState = atom({})
+export const userState = atom<UserStateType>({})
