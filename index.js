3a1a7c267c4d7f3427ca707148e9aae7d3321924
Bit Code Developer <developer@bitcode.pro>
2022-08-07 10:15:46 +0600

    Refact: Removed static file check

--- index.js
+++ index.js
@@ -10,32 +10,11 @@ const slasher = handler => (req, res) => {
     return handler(req, res)
 }
 exports.bitAssist = slasher((req, res) => {
-    if (req.url.startsWith('/_next/static/')) {
-        const file = '.' + req.url.replace('_next', '.next')
-        if (fs.existsSync(file)) {
-            // console.log(`Serving ${file}`)
-            res.setHeader('Cache-Control', 'public, max-age=0')
-            const mimeType = mime.lookup(file)
-            const body = fs.readFileSync(file)
-            return res.writeHead(200, {
-                'Content-Length': Buffer.byteLength(body),
-                'Content-Type': mimeType
-            })
-                .end(body)
-        }
-
-    } else {
-        return app.prepare()
-            .then(() => handle(req, res))
-            .catch(ex => {
-                console.error(ex.stack)
-                process.exit(1)
-            })
-    }
-    return res.writeHead(404, {
-        'Content-Length': Buffer.byteLength('404 Not Found'),
-        'Content-Type': 'text/plain'
-    })
-        .end('404 Not Found')
+    return app.prepare()
+        .then(() => handle(req, res))
+        .catch(ex => {
+            console.error(ex.stack)
+            process.exit(1)
+        })
 })
 
