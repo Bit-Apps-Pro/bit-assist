179d65a6bbb7505ab7b7816d99b90f08960609de
Shuvo Mohajan <shuvomohajan@gmail.com>
2022-08-09 13:46:49 +0600

    fixed: linkedin url

--- src/components/widgetChannels/channels/Linkedin.tsx
+++ src/components/widgetChannels/channels/Linkedin.tsx
@@ -11,7 +11,7 @@ const Linkedin = () => {
       prev.config[key] = value
 
       if (key === 'unique_id') {
-        prev.config.url = `https://linkedin/in/${value}`
+        prev.config.url = `https://linkedin.com/in/${value}`
       }
     })
   }
